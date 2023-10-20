97df54a39f84bf6c0f4fcca6c5abc0ad182b6279
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-10-20 21:06:02 +0200

    fix: update namespace

--- src/namespace/namespace.ts
+++ src/namespace/namespace.ts
@@ -1,14 +1,4 @@
 /* tslint:disable */
-// Type definitions for non-npm package HERE Maps API for JavaScript 3.1
-// Project: https://developer.here.com/
-// Definitions by: Joshua Efiong <https://github.com/Josh-ES>
-//                 Bernd Hacker <https://github.com/denyo>
-//                 Ferdinand Armbruster <https://github.com/fx88>
-//                 Vladimir Dashukevich <https://github.com/life777>
-//                 Daniel Schuba <https://github.com/DaSchTour>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.1
-
 export declare namespace H {
     /***** Map *****/
     /**
